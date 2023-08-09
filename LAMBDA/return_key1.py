340c39d91272835420a37a74630b61f6ef11e6d3
Santy Cierco <santidisturbios@gmail.com>
2023-08-09 13:17:29 +0200

    Update return_key1.py

--- LAMBDA/return_key1.py
+++ LAMBDA/return_key1.py
@@ -8,5 +8,5 @@ def lambda_handler(event, context):
     print("value1 = " + event['key1'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+    #return event['key1']  # Echo back the first key value
+    raise Exception('Something went wrong')

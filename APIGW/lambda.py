b9c9235e7a8bc8fe42f543de20aab071ffeda305
Santy Cierco <santidisturbios@gmail.com>
2023-08-09 19:11:35 +0200

    Update lambda.py

--- APIGW/lambda.py
+++ APIGW/lambda.py
@@ -1,6 +1,7 @@
 import json
 
 def lambda_handler(event, context):
+    print(event)       #comprobar en cloudwatch que el metodo obtenido es a traves de GET
     body = "Hello From Lambda!"
     statusCode = 200
     return {

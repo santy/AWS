2b00ab7c59e9b9257aae8ae16b4f1f37502697a4
Santy Cierco <santidisturbios@gmail.com>
2023-08-17 10:26:16 +0200

    Update return_key1.py

--- LAMBDA/return_key1.py
+++ LAMBDA/return_key1.py
@@ -9,4 +9,4 @@ def lambda_handler(event, context):
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
     #return event['key1']  # Echo back the first key value
-    raise Exception('Something went wrong') #invoke and see cloudwatch
+    raise Exception('Something went wrong') #invoke and see cloudwatch destination with condition to send to sns

bc96d66a1984f66d9a56682e37ccb72a008482ab
Santy Cierco <santidisturbios@gmail.com>
2023-08-21 11:25:06 +0200

    Update lambda_time_sleep.py

--- LAMBDA/lambda_time_sleep.py
+++ LAMBDA/lambda_time_sleep.py
@@ -1,7 +1,16 @@
 import json
 import os
+import time
+
+
+def connect_to_db():
+    time.sleep(2)
+    
+    
+connect_to_db()
+
 
 def lambda_handler(event, context):
     # TODO implement
-    time.sleep(2)
+    #connect_to_db()
     return os.getenv("ENTORNO_VARIABLE")
